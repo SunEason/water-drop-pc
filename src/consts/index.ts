712aeca6e341c578b5ce8d5af5749da06ed67f68
Eason Sun <ballentin133@outlook.com>
2024-11-27 23:23:33 +0800

    feat: 添加请求头

---
+++ src/consts/index.ts
@@ -0,0 +1,2 @@
+export const AUTH_TOKEN = 'auth_token'
+export const BASE_URL = 'http://localhost:3000/graphql'
--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,6 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN, BASE_URL } from '../consts'
+
+const httpsLink = createHttpLink({
+  uri: BASE_URL,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  link: authLink.concat(httpsLink),
   cache: new InMemoryCache(),
 })
