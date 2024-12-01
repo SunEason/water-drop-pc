54016571b1325bf5a4e7003f571999439023e22c
Eason Sun <ballentin133@outlook.com>
2024-12-01 20:59:42 +0800

    feat: 使用 import.meta.env

--- src/consts/index.ts
+++
@@ -1,2 +0,0 @@
-export const AUTH_TOKEN = 'auth_token'
-export const BASE_URL = 'http://localhost:3000/graphql'
--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,13 +1,13 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-import { AUTH_TOKEN, BASE_URL } from '../consts'
 
 const httpsLink = createHttpLink({
-  uri: BASE_URL,
+  uri: import.meta.env.VITE_GRAPHQL_URL,
 })
 
+console.log(import.meta.env)
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(AUTH_TOKEN)
+  const token = localStorage.getItem(import.meta.env.VITE_AUTH_TOKEN)
   return {
     headers: {
       ...headers,
